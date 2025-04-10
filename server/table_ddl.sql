74d57e781e9f0e20e8c245f59fc46b4febab11cc
유효림 <yhl8224@gmail.com>
2025-04-10 22:00:45 +0900

    특약사항 테이블 추가

--- server/table_ddl.sql
+++ server/table_ddl.sql
@@ -698,6 +698,22 @@ cancel: 취소',
 )
     comment '매물 리스트' collate = utf8mb3_unicode_ci;
 
+create or replace table special_terms
+(
+    st_seq       bigint auto_increment comment 'Special Terms 시퀀스'
+        primary key,
+    st_content   varchar(500)                         not null comment 'Special Terms 특약사항 내용',
+    st_use_count int      default 0                   not null comment 'Special Terms 사용 횟수',
+    st_is_public tinyint  default 1                   not null comment 'Special Terms 특약사항 공유 여부
+0: 공유안함 (개인만 사용)
+1: 공유 (모두 사용 가능)',
+    st_created   datetime default current_timestamp() not null comment 'Special Terms 생성일시',
+    st_updated   datetime default current_timestamp() not null comment 'Special Terms 수정일시',
+    constraint special_terms_st_seq_uindex
+        unique (st_seq)
+)
+    comment '특약사항 리스트';
+
 create or replace table stan_region_cd
 (
     SRC_seq             bigint auto_increment
